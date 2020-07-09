c25fe0f1c49d4c4cfc6791f6ed47046c42aead7d
FifcoStyle <fifcostyle@gmail.com>
2020-07-09 17:17:35 +0200

    added PlayerImmune exception

---
+++ src/main/java/io/github/fifcostyle/CraftManager/exceptions/PlayerImmuneException.java
@@ -0,0 +1,14 @@
+package io.github.fifcostyle.CraftManager.exceptions;
+
+public class PlayerImmuneException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5352309633135505399L;
+	
+	public PlayerImmuneException(String playerName) {
+		super(playerName);
+	}
+
+}
