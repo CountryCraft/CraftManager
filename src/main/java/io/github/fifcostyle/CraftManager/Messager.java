d1a4e7cc26c8909f379d6724bdaf72ef6d513449
FifcoStyle <fifcostyle@gmail.com>
2020-08-09 17:50:27 +0200

    Added more levels of messages

--- src/main/java/io/github/fifcostyle/CraftManager/Messager.java
+++ src/main/java/io/github/fifcostyle/CraftManager/Messager.java
@@ -2,6 +2,7 @@ package io.github.fifcostyle.CraftManager;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -18,11 +19,19 @@ public class Messager {
 	}
 	
 	public String format(final String key, final Object... args) {
-		return this.format(true, key, args);
+		return this.format(Level.INFO, key, args);
 	}
 	
-	public String format(final boolean prefix, final String key, final Object... args) {
-		String message = prefix ? (this.get("prefix") + this.get(key)) : this.get(key);
+	public String format(Level level, final String key, final Object... args) {
+		String message = "";
+		if (level == Level.INFO) {
+			message = (this.get("prefixInfo") + this.get(key));
+		} else if (level == Level.WARNING) {
+			message = (this.get("prefixWarn") + this.get(key));
+		} else if (level == Level.SEVERE) {
+			message = (this.get("prefixSevere") + this.get(key));
+		} else message = (this.get("prefixInfo") + this.get(key));
+		
 		for (int i = 0; i < args.length; ++i) {
 			message = message.replace("{" + i + "}", String.valueOf(args[i]));
 		}
@@ -30,6 +39,6 @@ public class Messager {
 	}
 	
 	public String prefix(final String msg) {
-		return ChatColor.translateAlternateColorCodes('&', this.get("prefix") + msg);
+		return ChatColor.translateAlternateColorCodes('&', this.get("prefixInfo") + msg);
 	}
 }
