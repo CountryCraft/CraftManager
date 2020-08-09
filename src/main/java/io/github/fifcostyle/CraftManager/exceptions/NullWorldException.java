52f973fcc283eac01ef8631628bbd56dcf06dbef
FifcoStyle <fifcostyle@gmail.com>
2020-08-09 17:52:18 +0200

    Added new Exceptions

---
+++ src/main/java/io/github/fifcostyle/CraftManager/exceptions/NullWorldException.java
@@ -0,0 +1,14 @@
+package io.github.fifcostyle.CraftManager.exceptions;
+
+public class NullWorldException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8199257219939553067L;
+	
+	public NullWorldException(String worldName) {
+		super(worldName);
+	}
+
+}
