d7bf4a0b33180f66e5b1948592ea85e2cf3094d9
FifcoStyle <fifcostyle@gmail.com>
2020-08-18 11:00:28 +0200

    changed Messager to use PrefixLevel

--- src/main/java/io/github/fifcostyle/CraftManager/Messager.java
+++ src/main/java/io/github/fifcostyle/CraftManager/Messager.java
@@ -2,11 +2,11 @@ package io.github.fifcostyle.CraftManager;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.logging.Level;
-
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+
+import io.github.fifcostyle.CraftManager.enums.PrefixLevel;
 
 public class Messager {
 	private FileConfiguration config;
@@ -19,17 +19,21 @@ public class Messager {
 	}
 	
 	public String format(final String key, final Object... args) {
-		return this.format(Level.INFO, key, args);
+		return this.format(PrefixLevel.INFO, key, args);
 	}
 	
-	public String format(Level level, final String key, final Object... args) {
+	public String format(PrefixLevel level, final String key, final Object... args) {
 		String message = "";
-		if (level == Level.INFO) {
+		if (level == PrefixLevel.INFO) {
 			message = (this.get("prefixInfo") + this.get(key));
-		} else if (level == Level.WARNING) {
+		} else if (level == PrefixLevel.WARNING) {
 			message = (this.get("prefixWarn") + this.get(key));
-		} else if (level == Level.SEVERE) {
-			message = (this.get("prefixSevere") + this.get(key));
+		} else if (level == PrefixLevel.ERROR) {
+			message = (this.get("prefixError") + this.get(key));
+		} else if (level == PrefixLevel.STAFF) {
+			message = (this.get("prefixStaff") + this.get(key));
+		} else if (level == PrefixLevel.DEBUG) {
+			message = (this.get("prefixDebug") + this.get(key));
 		} else message = (this.get("prefixInfo") + this.get(key));
 		
 		for (int i = 0; i < args.length; ++i) {
@@ -39,6 +43,16 @@ public class Messager {
 	}
 	
 	public String prefix(final String msg) {
-		return ChatColor.translateAlternateColorCodes('&', this.get("prefixInfo") + msg);
+		return this.prefix(PrefixLevel.INFO, msg);
+	}
+	public String prefix(PrefixLevel level, final String msg) {
+		String message = "";
+		if (level == PrefixLevel.INFO) message = (this.get("prefixInfo")) + msg;
+		else if (level == PrefixLevel.WARNING) message = (this.get("prefixWarn")) + msg;
+		else if (level == PrefixLevel.ERROR) message = (this.get("prefixError")) + msg;
+		else if (level == PrefixLevel.STAFF) message = (this.get("prefixStaff")) + msg;
+		else if (level == PrefixLevel.DEBUG) message = (this.get("prefixDebug")) + msg;
+		else message = this.get("prefixInfo") + msg;
+		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 }
